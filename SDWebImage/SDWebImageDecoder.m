f68992978c26f3cc4b9264a4e9ade4842e932f8e
Chris Wood <chris.wood@daydaycook.com>
2017-07-17 12:37:35 +0800

    Set maximum decoded image size to 2 MB

--- SDWebImage/SDWebImageDecoder.m
+++ SDWebImage/SDWebImageDecoder.m
@@ -65,7 +65,7 @@ static const size_t kBitsPerComponent = 8;
  * Suggested value for iPad2 and iPhone 4: 120.
  * Suggested value for iPhone 3G and iPod 2 and earlier devices: 30.
  */
-static const CGFloat kDestImageSizeMB = 60.0f;
+static const CGFloat kDestImageSizeMB = 2.0f;
 
 /*
  * Defines the maximum size in MB of a tile used to decode image when the flag `SDWebImageScaleDownLargeImages` is set
@@ -73,7 +73,7 @@ static const CGFloat kDestImageSizeMB = 60.0f;
  * Suggested value for iPad2 and iPhone 4: 40.
  * Suggested value for iPhone 3G and iPod 2 and earlier devices: 10.
  */
-static const CGFloat kSourceImageTileSizeMB = 20.0f;
+static const CGFloat kSourceImageTileSizeMB = 2.0f;
 
 static const CGFloat kBytesPerMB = 1024.0f * 1024.0f;
 static const CGFloat kPixelsPerMB = kBytesPerMB / kBytesPerPixel;
